3071f30ed19a2071381614785dee99d35681a5d7
greedy0110 <greedy0110@gmail.com>
2020-06-30 22:57:00 +0900

    INIT

---
+++ src/main/java/com/gmail/greedy0110/study/tacos/TacoCloudApplication.java
@@ -0,0 +1,13 @@
+package com.gmail.greedy0110.study.tacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacoCloudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gmail/greedy0110/study/tacos/TacoCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmail.greedy0110.study.tacos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
