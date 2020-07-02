c42587cc95b75dfef8630cd379f30bbc7e322a40
greedy0110 <greedy0110@gmail.com>
2020-07-02 21:14:20 +0900

    ADD:class Ingredient, Taco data class
    ADD:library lombok

---
+++ src/main/java/com/gmail/greedy0110/study/tacos/Taco.java
@@ -0,0 +1,10 @@
+package com.gmail.greedy0110.study.tacos;
+
+import lombok.Data;
+import java.util.List;
+
+@Data
+public class Taco {
+    private String name;
+    private List<String> ingredients;
+}
